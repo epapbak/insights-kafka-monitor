05113e5776bbda5fdb33dad257db636aea31491a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-21 14:27:43 +0100

    Unit tests for config.go

---
+++ config_test.go
@@ -0,0 +1,162 @@
+/*
+Copyright © 2022 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main_test
+
+// Unit test definitions for functions and methods defined in source file
+// config.go
+//
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/ccx-notification-writer/packages/config_test.html
+
+import (
+	"os"
+
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+
+	main "github.com/RedHatInsights/insights-kafka-monitor"
+)
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+}
+
+func mustLoadConfiguration(envVar string) {
+	_, err := main.LoadConfiguration(envVar, "tests/config1")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustFailLoadingConfigurationIfWrongEnvVar(envVar string) {
+	_, err := main.LoadConfiguration(envVar, "ANonExistingDefaultConfigPath")
+	if err == nil {
+		panic(err)
+	}
+}
+
+func removeFile(t *testing.T, filename string) {
+	err := os.Remove(filename)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustSetEnv(t *testing.T, key, val string) {
+	err := os.Setenv(key, val)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// TestLoadDefaultConfiguration loads a configuration file for testing
+func TestLoadDefaultConfiguration(t *testing.T) {
+	os.Clearenv()
+	mustLoadConfiguration("nonExistingEnvVar")
+}
+
+// TestLoadConfigurationFromEnvVariable tests loading the config. file for testing from an environment variable
+func TestLoadConfigurationFromEnvVariable(t *testing.T) {
+	os.Clearenv()
+
+	mustSetEnv(t, "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE", "tests/config2")
+	mustLoadConfiguration("INSIGHTS_KAFKA_MONITOR_CONFIG_FILE")
+}
+
+// TestLoadConfigurationNonEnvVarUnknownConfigFile tests loading an unexisting config file when no environment variable is provided
+func TestLoadConfigurationNonEnvVarUnknownConfigFile(t *testing.T) {
+	_, err := main.LoadConfiguration("", "foobar")
+	assert.Nil(t, err)
+}
+
+// TestLoadConfigurationBadConfigFile tests loading a bad config file when no environment variable is provided
+func TestLoadConfigurationBadConfigFile(t *testing.T) {
+	_, err := main.LoadConfiguration("", "tests/config3")
+	assert.Contains(t, err.Error(), `fatal error config file: While parsing config:`)
+}
+
+// TestLoadingConfigurationEnvVariableBadValueNoDefaultConfig tests loading a non-existent configuration file set in environment
+func TestLoadingConfigurationEnvVariableBadValueNoDefaultConfig(t *testing.T) {
+	os.Clearenv()
+
+	mustSetEnv(t, "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE", "non existing file")
+
+	_, err := main.LoadConfiguration("INSIGHTS_KAFKA_MONITOR_CONFIG_FILE", "")
+	assert.Contains(t, err.Error(), `fatal error config file: Config File "non existing file" Not Found in`)
+}
+
+// TestLoadingConfigurationEnvVariableBadValueNoDefaultConfig tests that if env var is provided, it must point to a valid config file
+func TestLoadingConfigurationEnvVariableBadValueDefaultConfigFailure(t *testing.T) {
+	os.Clearenv()
+
+	mustSetEnv(t, "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE", "non existing file")
+
+	_, err := main.LoadConfiguration("INSIGHTS_KAFKA_MONITOR_CONFIG_FILE", "tests/config1")
+	assert.Contains(t, err.Error(), `fatal error config file: Config File "non existing file" Not Found in`)
+}
+
+// TestLoadBrokerConfiguration tests loading the broker configuration sub-tree
+func TestLoadBrokerConfiguration(t *testing.T) {
+	envVar := "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE"
+
+	mustSetEnv(t, envVar, "tests/config2")
+	config, err := main.LoadConfiguration(envVar, "")
+	assert.Nil(t, err, "Failed loading configuration file from env var!")
+
+	brokerCfg := main.GetBrokerConfiguration(config)
+
+	assert.Equal(t, "localhost:29092", brokerCfg.Address)
+	assert.Equal(t, "ccx_test_notifications", brokerCfg.Topic)
+}
+
+// TestLoadLoggingConfiguration tests loading the logging configuration sub-tree
+func TestLoadLoggingConfiguration(t *testing.T) {
+	envVar := "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE"
+	mustSetEnv(t, envVar, "tests/config2")
+	config, err := main.LoadConfiguration(envVar, "")
+	assert.Nil(t, err, "Failed loading configuration file from env var!")
+
+	loggingCfg := main.GetLoggingConfiguration(config)
+
+	assert.Equal(t, true, loggingCfg.Debug)
+	assert.Equal(t, "", loggingCfg.LogLevel)
+}
+
+// TestLoadOutputConfiguration tests loading the output configuration sub-tree
+func TestLoadOutputConfiguration(t *testing.T) {
+	envVar := "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE"
+	mustSetEnv(t, envVar, "tests/config2")
+	config, err := main.LoadConfiguration(envVar, "")
+	assert.Nil(t, err, "Failed loading configuration file from env var!")
+
+	outputCfg := main.GetOutputConfiguration(config)
+
+	assert.Equal(t, true, outputCfg.Verbose)
+}
+
+// TestLoadConfigurationFromEnvVariableClowderEnabled tests loading the config.
+// file for testing from an environment variable. Clowder config is enabled in
+// this case.
+func TestLoadConfigurationFromEnvVariableClowderEnabled(t *testing.T) {
+	os.Clearenv()
+
+	mustSetEnv(t, "INSIGHTS_KAFKA_MONITOR_CONFIG_FILE", "tests/config2")
+	mustSetEnv(t, "ACG_CONFIG", "tests/clowder_config.json")
+	mustLoadConfiguration("INSIGHTS_KAFKA_MONITOR_CONFIG_FILE")
+}
